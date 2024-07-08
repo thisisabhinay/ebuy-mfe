d695ee2fe21b52af02fc220a47f6b2d2a04c8eb5
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-07-08 15:17:54 +0530

    build: setup nx monorepo with catalogue and checkout react apps

---
+++ ebuy-mfe/apps/catalogue/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'catalogue',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/catalogue',
+};
