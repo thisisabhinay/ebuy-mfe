bb97bed35314a463dd36fe1ff1cb03f6d30d9b25
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-07-08 23:33:07 +0530

    fix: export and import header component

--- ebuy-mfe/apps/catalogue/src/app/app.tsx
+++ ebuy-mfe/apps/catalogue/src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import styles from "./app.module.css";
+import NxWelcome from "./nx-welcome";
+import { Header } from "@ebuy/ui";
 
 export function App() {
   return (
     <>
+      <Header />
       <NxWelcome title="catalogue" />
       <div />
     </>
